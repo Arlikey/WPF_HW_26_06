948a72fd42a8c857a924af183be364441f18d69f
admin <admin@DESKTOP-FA5KDQG>
2024-07-04 13:16:30 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_HW_26_06
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void RoomCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var clickedElement = e.OriginalSource as Shape;
+            if (clickedElement != null)
+            {
+                MessageBox.Show($"Вы выбрали: {clickedElement.Tag}");
+            }
+        }
+    }
+}
